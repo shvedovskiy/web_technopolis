827417d5a1519749b11a35d97736ee77af292741
Oleg Shvedov <www.goo5@me.com>
2016-11-30 11:09:45 +0300

    Initial commit

---
+++ School/settings.py
@@ -0,0 +1,93 @@
+import os
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+SECRET_KEY = 'u+qex5$t*i93mn^q3-rpvk=@)*(gf^$^urn!y^#*s^r!ycle4_'
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'polls.apps.PollsConfig',
+    'course.apps.CourseConfig',
+    'blog.apps.BlogConfig',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'School.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),
+                 os.path.join(BASE_DIR, 'polls/templates'),
+                os.path.join(BASE_DIR, 'course/templates'),
+                 ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'School.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
---
+++ polls/urls.py
@@ -0,0 +1,21 @@
+from django.conf.urls import url
+from . import views
+
+app_name = 'polls'
+urlpatterns = [
+    url(r'^$',
+        views.IndexView.as_view(),
+        name='index'),  # /polls/
+
+    url(r'^(?P<pk>[0-9]+)/$',
+        views.DetailView.as_view(),
+        name='detail'),  # /polls/5/
+
+    url(r'^(?P<pk>[0-9]+)/results/$',
+        views.ResultsView.as_view(),
+        name='results'),  # /polls/5/results/
+
+    url(r'^(?P<question_id>[0-9]+)/vote/$',
+        views.vote,
+        name='vote'),  # /polls/5/vote/
+]
