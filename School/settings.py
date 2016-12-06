00feadc8649d6220d2bffa2ce57054288b2fc9ab
Oleg Shvedov <www.goo5@me.com>
2016-12-06 20:35:20 +0300

    add STATIC_ROOT

--- School/settings.py
+++ School/settings.py
@@ -98,7 +98,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'blog/static'),
 ]
